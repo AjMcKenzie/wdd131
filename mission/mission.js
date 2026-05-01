768bcedd717f378a0da52380a12ba2f15077eb52
AjMcKenzie <heldiggris7@gmail.com>
2026-04-30 19:02:07 -0600

    mission

--- mission/mission.js
+++ mission/mission.js
@@ -11,11 +11,6 @@ function changeTheme() {
         logo.src = 'byui-logo-white.png';
         body.style.backgroundColor = '#323332';
         body.style.color = 'white';
-    } else if (current == 'darker') {
-        // added in a darker theme option for fun to try out more changes to the colors and logo 
-        logo.src = 'byui-logo-white.png';
-        body.style.backgroundColor = 'black';
-        body.style.color = 'white';
     } else {
         // code for changes to colors and logo
         logo.src = 'byui-logo-blue.png';
