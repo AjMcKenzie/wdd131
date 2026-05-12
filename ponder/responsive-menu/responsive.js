f324bb61210631aad36da8c082a7c2cadb8ac1d4
Andrew McKenzie <heldiggris7@gmail.com>
2026-05-12 11:13:07 -0600

    Responsive nav

---
+++ ponder/responsive-menu/responsive.js
@@ -0,0 +1,17 @@
+let menuButton = document.getElementsByClassName("menu-btn")[0];
+
+console.log(menuButton);
+
+// event listener
+menuButton.addEventListener("click", handleMenuButtonClick);
+
+function handleMenuButtonClick(event) {
+    console.log(event);
+    // toggle on/off the menu display
+
+    //grab the nav from the DOM
+    let nav = document.querySelector("nav");
+    // toggle the .hide class on/off
+    nav.classList.toggle("hide");
+    menuButton.classList.toggle("change");
+}
