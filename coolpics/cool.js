0aefe6b91081072b177130f86ca2d1c9e049da29
AjMcKenzie <heldiggris7@gmail.com>
2026-05-16 15:29:56 -0600

    coolPics2

---
+++ coolpics/cool.js
@@ -0,0 +1,23 @@
+const gallery = document.querySelector('.gallery');
+const modal = document.querySelector('dialog');
+const modalImage = modal.querySelector('img');
+const closeButton = modal.querySelector('.close-viewer');
+
+gallery.addEventListener('click', openModal);
+
+function openModal(event) {
+    console.log(event.target.src);
+    let imageSrc = event.target.src;
+    modalImage.src = imageSrc.replace('sm', 'full');
+    modal.showModal();
+}
+
+closeButton.addEventListener('click', () => {
+    modal.close();
+});
+
+modal.addEventListener('click', (event) => {
+    if (event.target === modal) {
+        modal.close();
+    }
+});
