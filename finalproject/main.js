8a5e95bba050afbf19e429073f6008a7b57fad1a
AjMcKenzie <heldiggris7@gmail.com>
2026-07-17 16:24:13 -0600

    finalProjectUpdate

--- finalproject/main.js
+++ finalproject/main.js
@@ -3,7 +3,17 @@ const bookPegs = [
     {book: "2 Nephi", peg: "Basket"},
     {book: "Jacob", peg: "Cup"},
     {book: "Enos", peg: "Nose"},
-    {book: "Jarom", peg: "Jar"}
+    {book: "Jarom", peg: "Jar"},
+    {book: "Omni", peg: "Omniscient Satellite"},
+    {book: "Words of Mormon", peg: "Many Words"},
+    {book: "Mosiah", peg: "Mouse"},
+    {book: "Alma", peg: "Elm Tree"},
+    {book: "Helaman", peg: "Operating Room"},
+    {book: "3 Nephi", peg: "Group of People"},
+    {book: "4 Nephi", peg: "Harvest"},
+    {book: "Mormon", peg: "Anchor"},
+    {book: "Ether", peg: "Tire"},
+    {book: "Moroni", peg: "The Temple Angel Moroni"}
 ]
 
 const chapterPegs = [
@@ -28,44 +38,204 @@ const chapterPegs = [
     {chapter: "19", peg: "Teepee"},
     {chapter: "20", peg: "Henhouse"},
     {chapter: "21", peg: "Net"},
-    {chapter: "22", peg: "Onion"}
+    {chapter: "22", peg: "Onion"},
+    {chapter: "23", peg: "Gnome"},
+    {chapter: "24", peg: "Nero"},
+    {chapter: "25", peg: "Nile"},
+    {chapter: "26", peg: "Nudge"},
+    {chapter: "27", peg: "Nook"},
+    {chapter: "28", peg: "Navy"},
+    {chapter: "29", peg: "Knob"},
+    {chapter: "30", peg: "Mace"},
+    {chapter: "31", peg: "Mit"},
+    {chapter: "32", peg: "Moon"},
+    {chapter: "33", peg: "Mummy"},
+    {chapter: "34", peg: "Mower"},
+    {chapter: "35", peg: "Mule"},
+    {chapter: "36", peg: "Match"},
+    {chapter: "37", peg: "Mic"},
+    {chapter: "38", peg: "Muff"},
+    {chapter: "39", peg: "Mop"},
+    {chapter: "40", peg: "Rice"},
+    {chapter: "41", peg: "Road"},
+    {chapter: "42", peg: "Rain"},
+    {chapter: "43", peg: "Ram"},
+    {chapter: "44", peg: "Roar"},
+    {chapter: "45", peg: "Rail"},
+    {chapter: "46", peg: "Arch"},
+    {chapter: "47", peg: "Rock"},
+    {chapter: "48", peg: "Roof"},
+    {chapter: "49", peg: "Rope"},
+    {chapter: "50", peg: "Lassie"},
+    {chapter: "51", peg: "Loot"},
+    {chapter: "52", peg: "Lawn"},
+    {chapter: "53", peg: "Alamo"},
+    {chapter: "54", peg: "Lair"},
+    {chapter: "55", peg: "Lolli"},
+    {chapter: "56", peg: "Leach"},
+    {chapter: "57", peg: "Log"},
+    {chapter: "58", peg: "Lava"},
+    {chapter: "59", peg: "Lab"},
+    {chapter: "60", peg: "Cheese"},
+    {chapter: "61", peg: "Jet"},
+    {chapter: "62", peg: "Genie"},
+    {chapter: "63", peg: "Gym"},
 ];
 
 const chapterData = [
     {
-        book: bookPegs[0].book,
-        chapter: chapterPegs[0].chapter,
-        story: "One end of the neck tie is fastened to Nephi's Moving Van and the other is secured to the wall. The van pulls down the walls of Jerusalem.",
-        doctrine: "Lehi sees in vision that \nJerusalem will be destroyed.",
-        ref: ":4"
+        "book": bookPegs[0].book,
+        "chapter": chapterPegs[0].chapter,
+        "story": "One end of the neck tie is fastened to Nephi's Moving Van and the other is secured to the wall. The van pulls down the walls of Jerusalem.",
+        "doctrine": "Lehi sees in vision that \nJerusalem will be destroyed.",
+        "ref": ":4"
     },
     {
-        book: bookPegs[0].book,
-        chapter: chapterPegs[1].chapter,
-        story: "Noah loads the ark onto the top of Nephi's Moving van and drives away from Jerusalem.",
-        doctrine: "Lehi's family leaves Jerusalem ",
-        ref: ":4"
+        "book": bookPegs[0].book,
+        "chapter": chapterPegs[1].chapter,
+        "story": "Noah loads the ark onto the top of Nephi's Moving van and drives away from Jerusalem.",
+        "doctrine": "Lehi's family leaves Jerusalem",
+        "ref": ":4"
     },
     {
-        book: bookPegs[0].book,
-        chapter: chapterPegs[2].chapter,
-        story: "Ma kicks kids out of the back of Nephi's Moving van and sends them back to Jerusalem to retrieve the brass plates.",
-        doctrine: "Lehi is told in vision to send his children back to Jerusalem to retrieve the brass plates.",
-        ref: ":2"
+        "book": bookPegs[0].book,
+        "chapter": chapterPegs[2].chapter,
+        "story": "Ma kicks kids out of the back of Nephi's Moving van and sends them back to Jerusalem to retrieve the brass plates.",
+        "doctrine": "Lehi is told in vision to send his children back to Jerusalem to retrieve the brass plates.",
+        "ref": ":2"
     },
     {
-        book: bookPegs[0].book,
-        chapter: chapterPegs[3].chapter,
-        story: "The rays of light coming from the headlights of Nephi's Moving van turn into deadly laser weapons that cut off Laban's head.",
-        doctrine: "Nephi slays Laban.",
-        ref: ":18"
+        "book": bookPegs[0].book,
+        "chapter": chapterPegs[3].chapter,
+        "story": "The rays of light coming from the headlights of Nephi's Moving van turn into deadly laser weapons that cut off Laban's head.",
+        "doctrine": "Nephi slays Laban.",
+        "ref": ":18"
     },
     {
-        book: bookPegs[1].book,
-        chapter: chapterPegs[1].chapter,
-        story: "The Liberty Bell is formed from an ephah basket that is turned upside down and a neck tie forms the striker inside of the bell.",
-        doctrine: "The promised land will be a land of Liberty.",
-        ref: ":5"
+        "book": bookPegs[0].book,
+        "chapter": chapterPegs[4].chapter,
+        "story": "Lye soap is used to etch the contents of the brass plates into the side of Nephi's Moving van.",
+        "doctrine": "The contents of the Brass Plates are reviewed.",
+        "ref": ":10-14"
+    },
+    {
+        "book": bookPegs[0].book,
+        "chapter": chapterPegs[5].chapter,
+        "story": "On top of the moving van Nephi respectfully holds a shoe full of gold plates above his head as an act of pleasing God.",
+        "doctrine": "Nephi Writes to Please God.",
+        "ref": ":5"
+    },
+    {
+        "book": bookPegs[0].book,
+        "chapter": chapterPegs[6].chapter,
+        "story": "Cows are herded out of the back of Nephi's moving van and exchanged for the daughters of Ishmael that they have returned to Jerusalem to retrieve.",
+        "doctrine": "Lehi sends his sons back to Jerusalem to retrieve Ishmael's Family.",
+        "ref": ":2"
+    },
+    {
+        "book": bookPegs[0].book,
+        "chapter": chapterPegs[7].chapter,
+        "story": "The Darth Vader foe is standing on the hood of Nephi's moving van trying to look like the tree of life.",
+        "doctrine": "Lehi sees in a dream the tree of Life.",
+        "ref": "81-22:"
+    },
+    {
+        "book": bookPegs[0].book,
+        "chapter": chapterPegs[8].chapter,
+        "story": "Two sets of dinner plates are used to package a pie that is then stored in Nephi's moving van.",
+        "doctrine": "There are two sets of plates, the large and small plates of Nephi.",
+        "ref": "9:1-38"
+    },
+    {
+        "book": bookPegs[0].book,
+        "chapter": chapterPegs[9].chapter,
+        "story": "Nephi's moving van runs over a daisy. The daisy remains unaffected yesterday, today and forever.",
+        "doctrine": "God is the same yesterday, today & forever.",
+        "ref": ":18"
+    },
+    {
+        "book": bookPegs[0].book,
+        "chapter": chapterPegs[10].chapter,
+        "story": "Nephi tatoos (or draws) a large tree of life into the side of Nephi's moving van.",
+        "doctrine": "Nephi sees Lehi's vision of the tree of life.",
+        "ref": "11:1-36"
+    },
+    {
+        "book": bookPegs[0].book,
+        "chapter": chapterPegs[11].chapter,
+        "story": "Twain stands on the top of Nephi's moving van as the lookout who sees the land of promise.",
+        "doctrine": "Nephi sees in vision the land of promise.",
+        "ref": ":1"
+    },
+    {
+        "book": bookPegs[0].book,
+        "chapter": chapterPegs[12].chapter,
+        "story": "As Nephi's moving van bounces down the road many parts of the Bible are bounce out of the back of the van and into a tomb that is being pulled along behind the van. \nThe parts bounce on top of the head of Christopher Columbus who is standing inside the tomb.",
+        "doctrine": "Many plain and precious parts of the Bible are lost \nColumbus will discover America.",
+        "ref": ":28\n:12"
+    },
+    {
+        "book": bookPegs[0].book,
+        "chapter": chapterPegs[13].chapter,
+        "story": "Out of the tail pipe of Nephi's moving van tar is spit onto two churches.",
+        "doctrine": "In the last days there will be two churches only.",
+        "ref": ":10"
+    },
+    {
+        "book": bookPegs[0].book,
+        "chapter": chapterPegs[14].chapter,
+        "story": "Towel mud flaps on the back of Nephi's moving van keep the wheels from kicking mud onto the van and making it filthy. On the bottom of the van is printed the words, \"God's Kingdom\".",
+        "doctrine": "God's kingdom is not filthy.",
+        "ref": ":34"
+    },
+    {
+        "book": bookPegs[0].book,
+        "chapter": chapterPegs[15].chapter,
+        "story": "Nephi's moving van drives at the bottom of the ditch and is kept on the very correct ditch path and by the liahona on the hood.",
+        "doctrine": "The liahona guides their course in the wilderness.",
+        "ref": ":10,16"
+    },
+    {
+        "book": bookPegs[0].book,
+        "chapter": chapterPegs[16].chapter,
+        "story": "Nephi uses Nephi's moving van as a hammer that drives a very large tack into a very large ship.",
+        "doctrine": "Nephi begins building a ship.",
+        "ref": ":8"
+    },
+    {
+        "book": bookPegs[0].book,
+        "chapter": chapterPegs[17].chapter,
+        "story": "Nephi is bound to a T.V. inside the moving van inside of a ship that is then driven backwards.",
+        "doctrine": "Nephi is bound and the ship is driven back.",
+        "ref": ":11-13"
+    },
+    {
+        "book": bookPegs[0].book,
+        "chapter": chapterPegs[18].chapter,
+        "story": "Nephi's moving van is driven into a teepee where a very hot fire melts down the steel in the van to be made into 'plates of ore'.",
+        "doctrine": "Nephi moltens ore for use in making the plates.",
+        "ref": ":1"
+    },
+    {
+        "book": bookPegs[0].book,
+        "chapter": chapterPegs[19].chapter,
+        "story": "Nephi's moving van is driven into a henhouse that has been turned into a fiery 'furnace of affliction'. Being righteous, the furnace has to affect on the van.",
+        "doctrine": "The Lord refines us in the fiery furnace of affliction. - Isaiah 48",
+        "ref": ":10"
+    },
+    {
+        "book": bookPegs[0].book,
+        "chapter": chapterPegs[20].chapter,
+        "story": "A huge net is used to hold back Nephi's moving van to teach the van it will not be ashamed to wait.",
+        "doctrine": "The Lord tells us they will not be ashamed that wait for me. -Isaiah 49:23",
+        "ref": ":23"
+    },
+    {
+        "book": bookPegs[0].book,
+        "chapter": chapterPegs[21].chapter,
+        "story": "An open bed Nephi's moving van is filled with onions. As it bounces down the road onions spill out on the ground and then immediately bounce right back into the moving van.",
+        "doctrine": "Israel shall be scattered and then later gathered.",
+        "ref": ":3,12"
     }
 ];
 
@@ -73,7 +243,6 @@ const currentPage = window.location.pathname.split("/").pop();
 const content = document.getElementById("content");
 const navLinks = document.querySelectorAll("nav a");
 
-const flip = document.getElementById("flip");
 const next = document.getElementById("next");
 const prev = document.getElementById("prev");
 const random = document.getElementById("random");
@@ -82,12 +251,40 @@ let currentChapterIndex = 0;
 let currentBookIndex = 0;
 let showingFront = true;
 
+let menuButton = document.getElementsByClassName("menu-btn")[0];
+menuButton.addEventListener("click", handleMenuButtonClick);
 
-navLinks.forEach((link) => {
-    if (link.getAttribute("href") === currentPage) {
-        link.classList.add("active");
+let card = document.getElementById("card");
+let contentSection = document.getElementById("content");
+
+const chapterButtons = document.querySelectorAll("#numbers button");
+
+function getChapterCount() {
+    if (currentPage === "chapters.html") {
+        return chapterPegs.length;
+    } else {
+        return chapterData.length;
     }
-});
+}
+
+let bookButtons = document.querySelectorAll("#BOMbooks button");
+
+function getBookCount() {
+    if (currentPage === "books.html") {
+        return bookPegs.length;
+    } else {
+        return bookPegs.length;
+    }
+}
+
+
+if (navLinks.length > 0) {
+    navLinks.forEach((link) => {
+        if (link.getAttribute("href") === currentPage) {
+            link.classList.add("active");
+        }
+    });
+}
 
 function renderCard() {
     if (currentPage === "chapters.html") {
@@ -103,15 +300,6 @@ function renderCard() {
         const row = bookPegs[currentBookIndex];
         const frontText = `${row.book}`;
         const backText = row.peg;
-        if (showingFront) {
-            content.textContent = frontText;
-        } else {
-            content.textContent = backText;
-        }
-    } else if (currentPage === "visualize.html") {
-        const row = chapterData[currentChapterIndex];
-        const frontText = `${row.book} ${row.chapter}`;
-        const backText = `${row.story}`;
         if (showingFront) {
             content.textContent = frontText;
         } else {
@@ -138,9 +326,12 @@ function renderCard() {
     }
 }
 
-
-let menuButton = document.getElementsByClassName("menu-btn")[0];
-menuButton.addEventListener("click", handleMenuButtonClick);
+function randomize(){
+    currentChapterIndex = Math.floor(Math.random() * getChapterCount());
+    currentBookIndex = Math.floor(Math.random() * bookPegs.length);
+    showingFront = true;
+    renderCard();
+}
 
 function handleMenuButtonClick(event) {
     let nav = document.querySelector("nav");
@@ -148,32 +339,69 @@ function handleMenuButtonClick(event) {
     menuButton.classList.toggle("change");
 }
 
-flip.addEventListener("click", () => {
-    showingFront = !showingFront;
-    renderCard();
-});
+if(currentPage !== "index.html") {
 
-next.addEventListener("click", () => {
-    currentChapterIndex = (currentChapterIndex + 1) % chapterData.length;
-    currentBookIndex = (currentBookIndex + 1) % bookPegs.length;
-    showingFront = true;
-    renderCard();
-});
+    card.addEventListener("click", () => {
+        showingFront = !showingFront;
+        card.classList.toggle("flipped");
+        contentSection.classList.toggle("unflipped");
+    
+        renderCard();
+    });
+    
+    next.addEventListener("click", () => {
+        if (currentPage === "test.html") {
+            randomize();
+        }else{
+            currentChapterIndex = (currentChapterIndex + 1) % getChapterCount();
+            currentBookIndex = (currentBookIndex + 1) % bookPegs.length;
+            showingFront = true;
+            renderCard();
+        }
 
-prev.addEventListener("click", () => {
-    currentChapterIndex = (currentChapterIndex - 1 + chapterData.length) % chapterData.length;
-    currentBookIndex = (currentBookIndex - 1 + bookPegs.length) % bookPegs.length;
-    showingFront = true;
-    renderCard();
-});
+    });
+    
+    if (prev) {
+        prev.addEventListener("click", () => {
+            currentChapterIndex = (currentChapterIndex - 1 + getChapterCount()) % getChapterCount();
+            currentBookIndex = (currentBookIndex - 1 + bookPegs.length) % bookPegs.length;
+            showingFront = true;
+            renderCard();
+        });
+    }
+    
+    if (random) {
+        random.addEventListener("click", () => {
+            randomize();
+        });
+    }
 
-random.addEventListener("click", () => {
-    currentChapterIndex = Math.floor(Math.random() * chapterData.length);
-    currentBookIndex = Math.floor(Math.random() * bookPegs.length);
-    showingFront = true;
-    renderCard();
-});
+    chapterButtons.forEach((button, index) => {
+        button.addEventListener("click", () => {
+            currentChapterIndex = index;
+            showingFront = true;
+            card.classList.remove("flipped");
+            contentSection.classList.remove("unflipped");
+            renderCard();
+        });
+    });
 
-renderCard();
+    bookButtons.forEach((button, index) => {
+        button.addEventListener("click", () => {
+            currentBookIndex = index;
+            showingFront = true;
+            card.classList.remove("flipped");
+            contentSection.classList.remove("unflipped");
+            renderCard();
+        });
+    });
+
+    if (currentPage === "test.html") {
+        randomize();
+    }
+    
+    renderCard();
+}
+
 
 
